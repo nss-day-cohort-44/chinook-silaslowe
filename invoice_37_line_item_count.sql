394b0abb373e538a343dd7215a785e3bd1b5cf02
negative1skunk@gmail.com <silaslowe@gmail.com>
2021-02-26 09:58:23 -0600

    10

---
+++ invoice_37_line_item_count.sql
@@ -0,0 +1,13 @@
+SELECT
+    COUNT(il.invoicelineid) 
+FROM InvoiceLine il
+JOIN Invoice i
+    ON il.invoiceid = i.invoiceid
+WHERE i.InvoiceId = 37;
+
+-- Select COUNT(il.InvoiceId)
+-- From 
+--  InvoiceLine il, 
+--  Invoice i 
+-- ON i.InvoiceId = il.InvoiceId
+-- WHERE i.InvoiceId = 37
